005c1617bbf0c90b268e8682d5995e0cd2dfc959
jynadal <nadaljeanyves@hotmail.fr>
2020-04-27 14:13:41 +0200

    Idem 1/2 avec rectification next()

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get('/api/stuff/:id', (req, res, next) => {
   Thing.findOne({ _id: req.params.id })
   .then(thing => res.status(200).json(thing))
   .catch(error => res.status(404).json({ error}));
-  next();
+  
 });
 
 app.get('/api/stuff', (req, res, next) => {
