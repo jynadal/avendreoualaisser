cdd2874b26adfb6ea79867d43117611491fef87d
jynadal <nadaljeanyves@hotmail.fr>
2020-05-03 13:34:35 +0200

    Tuto Finish

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,9 +1,12 @@
 const Thing = require('../models/Thing');
+const fs = require('fs');
 
 exports.createThing = (req, res, next) => {
-    delete req.body._id;
+    const thingObject = JSON.parse(req.body.thing);
+    delete thingObject._id;
     const thing = new Thing({
-      ...req.body
+      ...thingObject,
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
     thing.save()
     .then(() => res.status(201).json({
@@ -13,8 +16,12 @@ exports.createThing = (req, res, next) => {
 }
 
 exports.modifyThing = (req, res, next) => {
+  const thingObject = req.file ? {
+    ...JSON.parse(req.body.thing),
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : { ...req.body };
     Thing.updateOne({ _id: req.params.id },
-      { ...req.body, _id: req.params.id })
+      { ...thingObject, _id: req.params.id })
     .then(() => res.status(200).json({ 
       message: 'Objet modifié !'}))
     .catch(error => 
@@ -22,12 +29,19 @@ exports.modifyThing = (req, res, next) => {
 }
 
 exports.deleteThing = (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({
-      message: 'Objet supprimé !'}))
-    .catch(error => 
-      res.status(400).json({ error }));
-}
+  Thing.findOne({_id: req.params.id })
+    .then(thing => {
+      const filename = thing.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () => {
+        Thing.deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({
+           message: 'Objet supprimé !'}))
+          .catch(error => 
+            res.status(400).json({ error }));
+      });
+    })
+    .catch(error => res.status(500).json({ error }));    
+};
 
 exports.getOneThing = (req, res, next) => {
     Thing.findOne({ _id: req.params.id })
--- routes/stuff.js
+++ routes/stuff.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 const stuffCtrl = require('../controllers/stuff');
 
 
 
-router.post('/', auth, stuffCtrl.createThing);
+router.post('/', auth, multer, stuffCtrl.createThing);
     
-router.put('/:id',auth, stuffCtrl.modifyThing);
+router.put('/:id',auth, multer, stuffCtrl.modifyThing);
   
 router.delete('/:id', auth, stuffCtrl.deleteThing);
   
